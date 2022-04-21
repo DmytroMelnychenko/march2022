d407f4938f71f4e768d1b3778c572c627d6f91a4
DmytroMelnychenko <meldimld@gmail.com>
2022-04-21 20:14:09 +0300

    hwjs1

---
+++ javascript/lesson-2/main.js
@@ -0,0 +1,90 @@
+/*- Є змінна х, якій ви надаєте довільне числове значення.
+    Якщо змінна x не дорівнює нулю, виведіть 'Вірно', інакше виведіть 'Невірно'. Перевірте  скрипт при a, що дорівнює 1, 0, -3*/
+
+/*let x = -3;
+if (x !==0) {
+    console.log(true)
+}
+else {
+    console.log(false)
+}*/
+
+
+
+
+/*- Дано змінну time яка рівна числу від 0 до 59. Потрібно написати код, який перевірить, до якої четверті години попадає число
+(в першу, другу, третю или четверту частину години).*/
+
+/*let time = +prompt('enter namber from 0 to 59');
+
+if (time >= 0 && 15 <= time) {
+    console.log(' 1 quoter');
+}
+else if (time >= 16 && 30 <= time) {
+    console. log('2 quoter');
+}
+
+else if (time >= 31 && 45 <= time) {
+    console. log('3 qouter');
+}
+else if (time >= 46 && 59 <= time) {
+    console. log('4 qouter');
+}*/
+
+
+
+/*- У змінній day дано якесь число від 1 до 31. Потрібно визначити, у яку половину(декаду) місяця потрапляє це число (у першу, другу чи третю).*/
+
+/*
+let day = +prompt('enter namber from 0 to 31');
+if (namber >= 0 || 10 <= namber) {
+    console.log('1 decada')
+}
+else if (namber >= 11 || 20 <= namber) {
+    console.log('2 decada')
+}
+else if (namber >= 21 || 31 <= namber) {
+    console.log('3 decada')
+}
+*/
+
+
+
+
+/*- Скласти розклад на тиждень за домопоги switch. Користувач вводить порядковий номер дня тижня і на екрані відображається інфа що заплановано на цей день (можна замість плану на день, назву дня англійською).*/
+
+
+/*let week = +prompt ('enter number of day');
+switch (week) {
+    case 1:
+        console.log('monday');
+        break;
+    case 2:
+        console.log('tuesday');
+        break;
+    case 3:
+        console.log('wednesday');
+        break;
+    case 4:
+        console.log('thursday');
+        break;
+    case 5:
+        console.log('friday');
+        break;
+    case 6:
+        console.log('saturday');
+        break;
+    case 7:
+        console.log('sunday');
+        break;
+    default:
+}*/
+
+
+
+
+
+/*
+    - Користувач вводить або має два числа.
+        Потрібно знайти та вивести максимальне число з тих двох .
+        Також потрібно врахувати коли введені рівні числа.*/

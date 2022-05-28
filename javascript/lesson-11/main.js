60e07150f8e099c8c132f544e6f1ba996fa82f7b
DmytroMelnychenko <meldimld@gmail.com>
2022-05-28 17:23:42 +0300

    hw

--- javascript/lesson-11/main.js
+++ javascript/lesson-11/main.js
@@ -1,20 +1,36 @@
 /*-створити форму з інпутами для name та age.
     При відправці форми записати об'єкт в localstorage*/
 
+/*const { name, age } = document.forms.form;
+const btn = document.getElementById('btn');
+const key = 'key';
 
-
-
-
-
-
-
-
-
-
-
-
+const save = (name,age) => {
+    const obj = {
+        name,
+        age
+    }
+    localStorage.setItem(key,JSON.stringify(obj))
+}
+btn.onclick = () => {
+    save(name.value, age.value);
+}*/
 
 
 /*
 -створити форму з інпутами для model,type та volume автівки.
     при відпарвці форми об'єкти зберігаються в масиві в локальному сховищі.*/
+
+const { model, type, volume } = document.forms.car;
+const btn = document.getElementById('btn');
+
+const key = 'key';
+
+const save = (model, type, volume) => {
+   const cars = JSON.parse(localStorage.getItem(key)) || [];
+   cars.push({ model, type, volume });
+   localStorage.setItem(key,JSON.stringify(cars))
+}
+btn.onclick = () => {
+    save(model.value, type.value, volume.value);
+}
